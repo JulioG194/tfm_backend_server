a86afae1f454376459b27631fa912e1d6ea29de4
Julio Gonzalez <julio.gonzalez2014a@gmail.com>
2024-01-28 19:32:54 -0500

    fix: config s3

--- src/config/s3/config.ts
+++ src/config/s3/config.ts
@@ -1,9 +1,7 @@
 import { S3Client } from "@aws-sdk/client-s3";
-import { fromIni } from "@aws-sdk/credential-provider-ini";
 import 'dotenv/config';
 
 export const s3Client = new S3Client({
   region: process.env.AWS_REGION ?? '',
-  credentials: fromIni({ profile: 'default' }),
 });
 
