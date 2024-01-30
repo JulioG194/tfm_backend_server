728b5410c3c5ef47354aea749c7d4df9b6e2daa7
Julio Gonzalez <julio.gonzalez2014a@gmail.com>
2024-01-29 23:04:56 -0500

    fix:pool connections

--- src/config/database/db.connection.ts
+++ src/config/database/db.connection.ts
@@ -7,9 +7,17 @@ const { username, password, uri, database, port } = databaseConfig;
 //const mongoURI = `mongodb+srv://${username}:${password}@${uri}/${database}`;
 const mongoURI = `mongodb://${username}:${password}@${uri}:${port}/${database}?authSource=admin`;
 
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  poolSize: 10, // Número de conexiones en el pool
+  serverSelectionTimeoutMS: 5000, // Tiempo de espera para la selección del servidor
+  socketTimeoutMS: 45000, // Tiempo de espera para las operaciones después de conectarse
+};
+
 const connection = async (): Promise<void> => {
     try {
-      await connect(mongoURI);
+      await connect(mongoURI, options );
       logger.info("Connected to MongoDB");
     } catch (error) {
       logger.error("Error connecting to MongoDB", error);
