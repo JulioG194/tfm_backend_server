fc48ee23f4fa412c92e65e813754e4b5255b5101
Julio Gonzalez <julio.gonzalez2014a@gmail.com>
2024-01-30 15:28:27 -0500

    fix: export

--- src/config/database/db.config.ts
+++ src/config/database/db.config.ts
@@ -1,7 +1,7 @@
 
 require('dotenv').config();
 
-interface DatabaseConfig {
+export interface DatabaseConfig {
     username: string;
     password: string;
     database: string;
@@ -9,12 +9,5 @@ interface DatabaseConfig {
     uri: string;
   }
   
-  const databaseConfig: DatabaseConfig = {
-    username: process.env.DB_USERNAME ?? '',
-    password: process.env.DB_PASSWORD ?? '',
-    database: process.env.DB_NAME ?? '',
-    port: process.env.DB_PORT?? '',
-    uri: process.env.DB_URI ?? '',
-  };
-  
-  export default databaseConfig;
+
+  //export default databaseConfig;
