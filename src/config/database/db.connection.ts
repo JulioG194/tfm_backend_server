5a78cc041656a41de060cf1bbe0300de8bfdf9f8
Julio Gonzalez <julio.gonzalez2014a@gmail.com>
2024-01-30 14:47:55 -0500

    fix: db

--- src/config/database/db.connection.ts
+++ src/config/database/db.connection.ts
@@ -1,18 +1,18 @@
 // import {connect} from 'mongoose';
-import databaseConfig from './db.config';
+// import databaseConfig from './db.config';
 /* import { logger } from '../../core/shared/Logger';
 import { BaseError } from '../../core/types/Responses/BaseError';
 import { HttpStatusCode } from '../../core/types/HttpStatusCode'; */
 
-const { username, password, uri, database, port } = databaseConfig;
+// const { username, password, uri, database, port } = databaseConfig;
 
 //const mongoURI = `mongodb+srv://${username}:${password}@${uri}/${database}`;
-const mongoURI = `mongodb://${username}:${password}@${uri}:${port}/${database}?authSource=admin`;
+//const mongoURI = `mongodb://${username}:${password}@${uri}:${port}/${database}?authSource=admin`;
 
-const options = {
-  serverSelectionTimeoutMS: 5000, // Tiempo de espera para la selección del servidor
-  socketTimeoutMS: 45000, // Tiempo de espera para las operaciones después de conectarse
-};
+//const options = {
+ // serverSelectionTimeoutMS: 5000, // Tiempo de espera para la selección del servidor
+ // socketTimeoutMS: 45000, // Tiempo de espera para las operaciones después de conectarse
+//};
 
 /* const connection = async (): Promise<void> => {
     try {
@@ -27,6 +27,6 @@ const options = {
     }
 };
  */
-const connectionStrs = { mongoURI, options }
+//const connectionStrs = { mongoURI, options }
   
-export default connectionStrs;
+// export default connectionStrs;
--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,25 @@ import uploadFileRoute from "./core/shared/upload/UploadFileRoute";
 import handleErrorMiddleware from './middlewares/handleErrorMiddleware';
 import { logger } from './core/shared/Logger';
 import { connect } from 'mongoose';
-import dbConnection from "./config/database/db.connection";
+import { DatabaseConfig } from './config/database/db.config';
+
+const databaseConfig: DatabaseConfig = {
+  username: process.env.DB_USERNAME ?? '',
+  password: process.env.DB_PASSWORD ?? '',
+  database: process.env.DB_NAME ?? '',
+  port: process.env.DB_PORT?? '',
+  uri: process.env.DB_URI ?? '',
+};
+
+
+const { username, password, uri, database, port: mongoport } = databaseConfig;
+
+const mongoURI = `mongodb://${username}:${password}@${uri}:${mongoport}/${database}?authSource=admin`;
+
+const options = {
+  serverSelectionTimeoutMS: 5000, // Tiempo de espera para la selección del servidor
+  socketTimeoutMS: 45000, // Tiempo de espera para las operaciones después de conectarse
+};
 
 const app: Express = express();
 const port = process.env.PORT ?? 5000;
@@ -17,7 +35,7 @@ const port = process.env.PORT ?? 5000;
 async function startServer() {
 try {
 //await connection();
-/* await connect(dbConnection.mongoURI, dbConnection.options );
+await connect(mongoURI, options );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(securityMiddleware);
@@ -38,12 +56,13 @@ app.use((err: any, req: any, res:any, next: any) => {
   console.error(err);
   const statusCode = err.statusCode ?? 500;
   res.status(statusCode).json({ error: "Error Interno", message: err.message });
-}); */
+});
 
     app.listen(port, () => {
       logger.info(`[server]: Server is running at http://localhost:${port}`);
     });
   } catch (error) {
+    console.log('error', error);
     logger.error("Failed to connect to MongoDB", error);
   }
 }
